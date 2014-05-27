773bb63ae7e3742e4acb34bf4d3d77dac0178954
awtauer <atauer2015@kellogg.northwestern.edu>
2014-05-26 20:52:24 -0500

    Added associations
    Method Tables

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,9 @@
 class Photo < ActiveRecord::Base
+ validates :source, :presence => true
+
+ 	has_many :comments
+ 	belongs_to :user
+
+	has_many :favorites
+	has_many :fans, :through => :favorites, :source => :user
 end
